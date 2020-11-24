a1e5759a31cb47e81e4a4f1c7e1f6150a9bf4887
LoudLasagna <63579498+LoudLasagna@users.noreply.github.com>
2020-11-24 21:26:03 +0300

    изменения главной страницы, страницы продуктов
    Поменял пару штук на главной, добавил слайдер, панель для входа. Добавил странице продуктов хедер, футер, панель входа, убрал повторяющиеся в main стили из stylesForQuery.

---
+++ CUMPHONE/scripts/main.js
@@ -0,0 +1,47 @@
+
+var sliderElements = document.getElementsByClassName('sl');
+var sliderCircles = document.getElementsByClassName('sl__dots-dot');
+
+
+var currentIndex = 0;
+sliderElements[currentIndex].style.zIndex = 2;
+sliderCircles[currentIndex].firstChild.style.fill = 'black';
+
+
+for (var i = 1; i < sliderElements.length; i++)
+{
+	sliderElements[i].style.zIndex = -1;
+	sliderElements[i].style.opacity = 0;
+}
+
+
+setInterval(increaseIndex, 3000);
+
+function increaseIndex(){
+	var inbex = currentIndex;
+	
+	inbex+=1;
+	if (inbex == sliderElements.length) inbex = 0;
+	
+	slide(inbex);
+}
+
+function slide(index)
+{
+	sliderElements[currentIndex].style.zIndex = -1;
+	sliderElements[currentIndex].style.opacity = 0;
+	sliderCircles[currentIndex].firstChild.style.fill = 'none';
+	
+	sliderElements[index].style.zIndex = 2;
+	sliderElements[index].style.opacity = 1;
+	sliderCircles[index].firstChild.style.fill = 'black';
+	
+	currentIndex = index;
+}
+
+
+
+
+document.getElementsByClassName('page__content-text text-title content-bottom link')[0].onclick = function() {
+window.location.href = 'products.html';
+};
