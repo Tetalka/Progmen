17c3a16d33e7056b17df842e514fe36dccad7be0
Pyotr <PrincesstaKamchapki@gmail.com>
2021-01-02 10:51:55 +0300

    Improve functions and structure
    Make appropriate changes to DB.php

--- CUMPHONE/GetPhones.php
+++ CUMPHONE/GetPhones.php
@@ -1,115 +1,104 @@
 <?php 
 
-	$user = 'id15446174_progman';
-	$pass = 'Progaman4.5~';
-	$db = 'id15446174_progmen';
-
-function getAll($filter = '') {
-    global $user, $pass, $db;
-        $phones = array();
-    $link = database('localhost', $user, $pass, $db);
-    $query = 'SELECT Model, Diagonal, Display, Resolution, Processor, Cores, ROM, RAM, Camera, Face_image, Price FROM `Products`';
-    if($filter !== '') {
-        $query = $query . ' WHERE Model LIKE "%' . $filter . '%"';
-    }
-        if ($result = $link->query($query . ";")) {
-            if($result->num_rows == 0) {
-                return false;
-            }
-            else {
-            $i = 0;
-            while($phone = $result->fetch_array(MYSQLI_ASSOC)) {
-                $phones[$i] = new stdclass;
-                $phones[$i]->Model = $phone['Model'];
-                $phones[$i]->Display = $phone['Diagonal'] . ' ' . $phone['Display'] . ' ' . $phone['Resolution'];
-                $phones[$i]->Processor = $phone['Processor'] . ' (' . $phone['Cores'] . ' cores)';
-                $phones[$i]->ROM = $phone['ROM'];
-                $phones[$i]->RAM = $phone['RAM'];
-                $phones[$i]->Camera = $phone['Camera'];
-                $phones[$i]->Face_image = $phone['Face_image'];
-                $phones[$i]->Price = $phone['Price'];
-                $i++;
-            }
-        return $phones;
-        }
-    }
-    else {
-    	say('Error', 'Server_error');
-    	writeError('Query error (' . $link->errno . ') ' . $link->error);
-    	}
+class Product {
+	function __construct($model, $price) {
+		$this->Model = $model;
+		$this->Price = $price;
+	}
 }
 
-function getModel($model, $color) {
-    if($color == null) $color = 'Black';
-        global $user, $pass, $db;
-        $link = database('localhost', $user, $pass, $db);
-        if($result = $link->query("SELECT
-    * FROM Phones WHERE Model = '" . $model . "' ;")) {
-            if($result == null) {
-               return 'Model_doesn\'t_exist'; 
-            }
-            else {
-                $phone = new stdclass;
-                $phone->Model = $model;
-                $phone->Color = $color;
-                $images = array();
-                $colors = array();
-                $colorsCode = array();
-                $i = 0;
-                while($row = $result->fetch_array(MYSQLI_ASSOC)) {
-                    if($row['Color'] == $color && strpos($row['Path'], 'main') === false) $images[count($images)] = $row['Path'];
-                    if(!arrayContains($colors, $row['Color'])) {
-                        $colors[$i] = $row['Color'];
-                        $colorsCode[$i] = $row['Value'];
-                    }
-                    $phone->Diagonal = $row['Diagonal'];
-                    $phone->Resolution = $row['Resolution'];
-                    $phone->Display = $row['Display'];
-                    $phone->Processor = $row['Processor'];
-                    $phone->ROM = $row['ROM'];
-                    $phone->RAM = $row['RAM'];
-                    $phone->Cores = $row['Cores'];
-                    $phone->Videoprocessor = $row['Videoprocessor'];
-                    $phone->Camera = $row['Camera'];
-                    $phone->Front_camera = $row['Front camera'];
-                    $phone->Wireless_interface = $row['Wireless interface'];
-                    $phone->Unit_of_WiFi = $row['Unit of Wi-Fi'];
-                    $phone->Numbers_of_SIMCard = $row['Numbers of SIM-card'];
-                    $phone->Type_of_SIM = $row['Type of SIM'];
-                    $phone->Battery = $row['Battery'];
-                    if($color != $row['MainColor']) {
-                        if(strpos($row['Path'], 'main') !== false && $color == $row['Color']) $phone->Face_image = $row['Path'];
-                    }
-                    else $phone->Face_image = $row['Face_image'];
-                    $phone->Price = $row['Price'];
-                    $i++;
-                }
-                $phone->Images = $images;
-                $phone->Colors = $colors;
-                $phone->Colors_value = $colorsCode;
-                return $phone;
-            }
-        }
-        else {
-            say('Error', 'Server_error');
-    	    writeError('Query error (' . $link->errno . ') ' . $link->error);
-        }
+function getAll($filter = '') {
+    global $progman;
+    $phones = array();
+    //$link = database($progman);
+    $query = 'CALL GetAllPhones("' . $filter . '");';
+	//$result = makeQuery($link, $query);
+	$result = $progman->makeQuery($query);
+    if($result === false || $result->num_rows == 0) return false;
+    else {
+		$i = 0;
+        while($phone = $result->fetch_array(MYSQLI_ASSOC)) {
+			$phones[$i] = new Product($phone['Model'], $phone['Price']);
+			$phones[$i]->Display = $phone['Diagonal'] . ' ' . $phone['Display'] . ' ' . $phone['Resolution'];
+			$phones[$i]->Processor = $phone['Processor'] . ' (' . $phone['Cores'] . ' cores)';
+			$phones[$i]->ROM = $phone['ROM'];
+			$phones[$i]->RAM = $phone['RAM'];
+			$phones[$i]->Camera = $phone['Camera'];
+			$phones[$i]->Price = $phone['Price'];
+			$phones[$i]->Image = $phone['Path'];
+			$i++;
+		}
+        return $phones;
+    }
+}
+
+function getModel($model, $color = null) {
+    global $progman;
+	$query = 'CALL GetPhoneByModel("' . $model . '");';
+	$result = $progman->makeQuery($query);
+	if($result === false || $result->num_rows == 0) return false;
+    $colors = array();	
+	$images = array();
+	$row1 = $result->fetch_array(MYSQLI_ASSOC);
+	$phone = new Product($model, $row1['Price']);
+	if($color == null) $color = $row1['Color'];
+	$phone->Diagonal = $row1['Diagonal'];
+    $phone->Resolution = $row1['Resolution'];
+    $phone->Display = $row1['Display'];
+    $phone->Processor = $row1['Processor'];
+	$phone->ROM = $row1['ROM'];
+    $phone->RAM = $row1['RAM'];
+    $phone->Cores = $row1['Cores'];
+	$phone->Videoprocessor = $row1['Videoprocessor'];
+	$phone->Camera = $row1['Camera'];
+    $phone->FrontCamera = $row1['Front camera'];
+    $phone->WirelessInterface = $row1['Wireless interface'];
+    $phone->UnitOfWiFi = $row1['Unit of Wi-Fi'];
+    $phone->NumbersOfSIMCard = $row1['Numbers of SIM-card'];
+    $phone->TypeOfSIM = $row1['Type of SIM'];
+    $phone->Battery = $row1['Battery'];
+	$phone->CurrentColor = $color;
+	$colors[$row1['Color']] = $row1['Color_Code'];
+	if($row1['Color'] == $color) $images[$row1['Image_Rank']] = $row1['Path'];
+	while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+		if(!arrayContains($colors, $row['Color'])) $colors[$row['Color']] = $row['Color_Code'];
+		if($row['Color'] == $color) $images[$row['Image_Rank']] = $row['Path'];
+	}
+    $phone->Images = $images;
+    $phone->Colors = $colors;
+    return $phone;
 }
 
 $request = file_get_contents('php://input');
 $request = json_decode($request, true);
-if($request['type'] == 'GetPhones') {
-    $request = $request['object'];
-    require 'DB.php';
-	if($request['model'] == 'all') {
-	    if(isset($request['filter'])) {
-	        $response = getAll(substr($request['filter'], 0, 60));
-	        if($response) say('OK', $response);
-	        else say('Error', 'No such models');
+require 'DB.php';
+$response = new Response();
+if($request['Type'] == 'GetPhones') {
+    $request = $request['Data'];
+	if($request['Model'] == 'All') {
+		$data;
+	    if(isset($request['Filter'])) {
+			$data = getAll(substr($request['Filter'], 0, 60));
 	    }
-	    else say('OK', getAll());
+	    else {
+			$data = getAll();
+		}
+		if(!$data) {
+			$response->writeError('No such models');
+		}
+		else $response->writeData($data);
 	}
-	else if ($request['model']) say('OK', getModel($request['model'], $request['color']));
+	else {
+		if ($request['Model']) {
+			if(isset($request['Color'])) $data = getModel($request['Model'], $request['Color']);
+			else $data = getModel($request['Model']);
+			if($data) $response->writeData($data);
+			else $response->writeError('No such model');
+		}
+	}
 }
-else say('Error', 'Bad_request');
+else {
+	$response->writeError('Bad_request');
+}
+$response->send();
 ?>
