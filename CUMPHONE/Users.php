895ae860fd99ea99a0c58a74ce513dbb254ebc9b
Pyotr <PrincesstaKamchapki@gmail.com>
2021-01-02 10:55:50 +0300

    Users.php: Improve function and sctructure
    Use of cookie, sending response

--- CUMPHONE/Users.php
+++ CUMPHONE/Users.php
@@ -1,111 +1,76 @@
 <?php
 
-	$user = 'id15446174_progman';
-	$pass = 'Progaman4.5~';
-	$db = 'id15446174_progmen';
-
-    /*$cookie_signIn = array(
-        'path' => '/',
-        'domain' => 'progmen.000webhostapp.com',
-        'secure' => true,
-        'httponly' => true,
-        'samesite' => 'Lax'
-    );*/
-
 function SignIn($login, $password) {
-    global $user, $pass, $db, $cookie_signIn;
-    $link = database('localhost', $user, $pass, $db);
-    $response = new stdClass;
-    if($_COOKIE['SignIn_times'] > 4) {
-        $response->Value = ['Too much attempts of login in', 'Try again in 15 minutes'];
-        say('Error', $response);
-    }
-    else {
-    if($result = $link->query("SELECT Login, Password FROM Clients WHERE Login = '" . $login . "';")) {
-		if($result->num_rows > 0) {
-			$value = $result->fetch_array(MYSQLI_ASSOC);
-			if(password_verify($password, $value['Password'])) {
-				$link->close();
-				$response->Login = $login;
-				say('Authorisation', $response);
-			}
-			else {
-			    setcookie('SignIn_times', $_COOKIE['SignIn_times']+1, time() + 900, '/', '', true, true);
-			    $response->Value = 'Invalid username or password';
-				say('Error', $response);
-			}
+    global $progman;
+	$user = $progman->makeQuery("SELECT Login, Password FROM Clients WHERE Login = '" . $login . "';");
+    if($user->num_rows != 0) {
+		$value = $user->fetch_array(MYSQLI_ASSOC);
+		if(password_verify($password, $value['Password'])) {
+			return $value['Login'];
 		}
-		else {
-		   	setcookie('SignIn_times', $_COOKIE['SignIn_times']+1, time() + 900, '/', '', true, true);
-		    $response->Value = 'Invalid username or password';
-			say('Error', $response);
-		}
-		}
-		else {
-		write_error($link->error);
-		$response->Value = 'Server_error';
-		say('Error', $response);
-		}
-    }
+	}
+	return false;
 }
 
 function SignUp($login, $password, $vpassword) {
-    global $user, $pass, $db;
-    $response = new stdClass;
-    if($password == $vpassword) {
+    global $progman;
     $password = password_hash($password, PASSWORD_DEFAULT, ['memory_cost' => 2048, 'time_cost' => 4, 'threads' => 4]);
-    $link = database('localhost', $user, $pass, $db);
-        if($result = $link->query('SELECT Login FROM Clients WHERE Login = \'' . $login . '\';')) {
-            if($result->num_rows > 0) {
-                $response->Value = 'Login already exists';
-                say('Error', $response);
-            }
-            else {
-                $result->free();
-                if($link->query('INSERT INTO Clients VALUES (NULL, "' . $login . '", "' . $password . '");')) {
-                    setcookie('SignUp_times', $_COOKIE['SignUp_times']+1, time() + 86400, '/', '', true, true);
-                    say('Registration');
-                }
-                else {
-                    $response->Value = 'Server_error';
-                    say('Error', $response);
-    	            writeError('Insert error (' . $link->errno . ') ' . $link->error);
-                }
-            }
-        }
-        else {
-            $response->Value = 'Server_error';
-            say('Error', $response);
-    	   writeError('Insert error (' . $link->errno . ') ' . $link->error);
-        }
+    $user = $progman->makeQuery('SELECT Login FROM Clients WHERE Login = \'' . $login . '\';');
+    if($user->num_rows != 0) {
+        return false;
     }
     else {
-        $response->Value = 'Passwords don\'t match';
-        say('Error', $response);
+        $progman->makeQuery('INSERT INTO Clients VALUES (NULL, "' . $login . '", "' . $password . '");');
+		return true;
     }
 }
 
 $request = file_get_contents('php://input');
 $request = json_decode($request, true);
 require 'DB.php';
+$response = new Response();
 if($request['Type'] == 'Users') {
-    $request = $request['data'];
+    $request = $request['Data'];
     $login = substr($request['Login'], 0, 55);
     $password = substr($request['Password'], 0, 55);
 	if($request['Action'] == 'Sign in') {
-	    if (!isset($_COOKIE['SignIn_times']))
-    	setcookie('SignIn_times', 0, time() + 900, '/', '', true, true);
-    	SignIn($login, $password);
+	    if (!isset($_COOKIE['SignIn_times'])) {
+			setcookie('SignIn_times', 0, time() + 900, '/', '', true, true);
+		}
+		if($_COOKIE['SignIn_times'] >= 5) {
+			$response->writeError('Too much attempts of login in'); 
+			$response->writeError('Try again in 15 minutes');
+		}	
+		else {
+			$status = SignIn($login, $password);
+			if($status) $response->writeData($status);
+			else {
+				setcookie('SignIn_times', $_COOKIE['SignIn_times']+1, time() + 900, '/', '', true, true);
+				$response->writeError('Invalid login or password');
+			}
+		}
     }
 	else if ($request['Action'] == 'Sign up') {
 	   $vpassword = substr($request['VPassword'], 0, 55);
-	   	   if (!isset($_COOKIE['SignUp_times'])) setcookie('SignUp_times', 0, time() + 86400, '/', '', true, true);
-	       if($_COOKIE['SignUp_times'] > 2) say('Error', ['Too much times of registrations', 'You can repeat it again in 1 day']);
-	       else {
-	           SignUp($login, $password, $vpassword);
-	       }
+	   	if (!isset($_COOKIE['SignUp_times'])) setcookie('SignUp_times', 0, time() + 86400, '/', '', true, true);
+	    if($_COOKIE['SignUp_times'] >= 3) {
+			$response->WriteError('Too much times of registrations');
+			$response->WriteError('You can repeat it again in 1 day');
+		}
+	    else {
+			if($password === $vpassword) {
+				$status = SignUp($login, $password, $vpassword);
+				if($status) {
+					setcookie('SignUp_times', $_COOKIE['SignUp_times']+1, time() + 86400, '/', '', true, true);
+					$response->writeData(true);
+				}
+				else $response->writeError('User already exists');
+			}
+			else $response->writeError('Passwords don\'t match');
 	    }
-	else say('Error', 'Bad_request');
+	}
+	else $response->WriteError('Bad_request');
 }
-else say('Error', 'Bad_request');
+else $response->WriteError('Bad_request');
+$response->send();
 ?>
