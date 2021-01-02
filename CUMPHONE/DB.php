1070d32f13413cf22dbefa21208855f5337fe282
Pyotr <PrincesstaKamchapki@gmail.com>
2021-01-02 10:44:48 +0300

    Make class for work with database
    Work with databases become easier

--- CUMPHONE/DB.php
+++ CUMPHONE/DB.php
@@ -10,15 +10,48 @@
     return $link;
     }
 }*/
-function database($url, $user, $pass, $db) {
-    $link = new mysqli($url, $user, $pass, $db);
-    if ($link->connect_error) {
-    		$db_error_reason = 'Connect Error (' . $link->connect_errno . ') ' . $link->connect_error;
-    		writeError($db_error_reason);
-            say('Error', 'Server_error');
-        }
-    return $link;
-};
+class Database {
+	function __construct($url, $user, $pass, $db) {
+		$this->url = $url;
+		$this->user = $user;
+		$this->pass = $pass;
+		$this->db = $db;
+	}
+	
+	function makeLink() {
+		$this->link = new mysqli($this->url, $this->user, $this->pass, $this->db);
+		if ($this->link->connect_error) {
+				$db_error_reason = 'Connect Error (' . $this->link->connect_errno . ') ' . $this->link->connect_error;
+				$this->writeError($db_error_reason);
+				//say('Error', 'Server_error');
+				return false;
+			}
+		return true;
+	}
+	
+	function makeQuery($query) {
+		if($this->makeLink()) {
+			if($result = $this->link->query($query)) {
+				$this->link->close();
+				return $result;
+			}
+			else {
+				$this->writeError('Query error (' . $this->link->errno . ') ' . $this->link->error);
+				$this->link->close();
+				return false;
+			}
+		}
+		else return false;
+	}
+	
+	function writeError($error) {
+    	$log = fopen('Errors.log', 'a');
+    	fwrite($log, "\n" . date('F, D, \D\a\y j, H:i:s: ') . $error . '.');
+    	fclose($log);
+	}
+}
+
+$progman = new Database('localhost', 'id15446174_progman', 'Progaman4.5~', 'id15446174_progmen');
 
 function arrayContains($array, $string) {
     for($i = 0; $i < count($array); $i++) {
@@ -32,11 +65,5 @@ function say($type = null, $message = null) {
     echo json_encode(['Type' => $type, 'Value' => $message]);
     }
     else echo json_encode(['Type' => $type]);
-}
-
-function writeError($error) {
-    	$log = fopen('Errors.log', 'a');
-    	fwrite($log, "\n" . date('F, D, \D\a\y j, H:i:s: ') . $error . '.');
-    	fclose($log);
 }
 ?>
